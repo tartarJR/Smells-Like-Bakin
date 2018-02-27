48097aa6fb4867a75ea143a890fa22452e47217c
tartarJR <mustafaogun.ozturk@gmail.com>
2018-02-27 19:22:27 +0300

    added recyclerview and changed app theme

--- app/src/main/java/com/tatar/smellslikebakin/ListFragment.java
+++ app/src/main/java/com/tatar/smellslikebakin/ListFragment.java
@@ -3,6 +3,8 @@ package com.tatar.smellslikebakin;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,13 +16,19 @@ import android.view.ViewGroup;
 
 public class ListFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private ListAdapter listAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        Log.i("FGRAGZ", container.toString());
-
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
